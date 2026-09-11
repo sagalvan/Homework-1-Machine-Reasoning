e464909fd3247e59714b31b4bf82bc35c4ed96e8
sagalvan <sagalvan@davidson.edu>
2026-09-11 09:00:08 -0400

    Temporarily updated board
    Replaced tile objects with int while the Tile class is being created

--- Board.java
+++ Board.java
@@ -1,24 +1,24 @@
 public class Board {
-    private Tile[] tiles;
+    private int[] tiles;
 
     // Initializes a board object
-    public Board(Tile[] tiles) {
+    public Board(int[] tiles) {
         // TODO: initialize tiles
         this.tiles = tiles;
     }
 
     // Returns the tile at the specified row and column
-    public Tile getTile(int row, int col) {
+    public int getTile(int row, int col) {
         // TODO: implement
         return tiles[row * 3 + col];
     }
 
     // Returns the empty tile
-    public Tile getEmpty() {
+    public int getEmpty() {
         // TODO: implement
         for (int row = 0; row < 3; row++){
             for (int col = 0; col < 3; col ++){
-                if (tiles[row * 3 + col].data == null) {
+                if (tiles[row * 3 + col] == 0) {
                     return tiles[row * 3 + col];
                 }
             }
